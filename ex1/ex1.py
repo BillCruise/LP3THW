08154a96d6cb5284daf0609a54503a5bbe2b6a75
Bill Cruise <cruise02@gmail.com>
2014-11-01 12:42:59 -0400

    LPTHW Exercise 1

---
+++ ex1/ex1.py
@@ -0,0 +1,8 @@
+# LPTHW ex1.py
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing!')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
