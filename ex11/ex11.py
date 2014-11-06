5d679c4f3da91a00c1deaf31e8f6ffac8181bc8b
Bill Cruise <cruise02@gmail.com>
2014-11-06 07:05:17 -0500

    LPTHW ex11.py - Asking Questions

---
+++ ex11/ex11.py
@@ -0,0 +1,11 @@
+# LPTHW ex11.py - Asking Questions
+
+print("How old are you?"),
+age = input()
+print("How tall are you?"),
+height = input()
+print("How much do you weigh?"),
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight))
