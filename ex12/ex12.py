0b15f452b54b1ad9a0de1ae9f1640fc95bb816cf
Bill Cruise <cruise02@gmail.com>
2014-11-07 07:07:53 -0500

    LPTHW ex12.py - Prompting People

---
+++ ex12/ex12.py
@@ -0,0 +1,9 @@
+# LPTHW ex12.py - Prompting People
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("So, you're %r old, %r tall and %r heavy." % (
+	age, height, weight))
+	
